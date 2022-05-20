19bcb5d2ac6d6c19cae7f6042cba0a596594ce88
teise <teisteu@gmail.com>
2022-05-20 13:31:03 +1200

    help page update3

--- 01_help_GUI_v2.py
+++ 01_help_GUI_v2.py
@@ -2,20 +2,19 @@ from tkinter import *
 from tkinter import ttk
 import random
 
-import self as self
 
 
 class Converter:
     def__init__(self):
-
     #Formatting variables....
-    background_colour = "yellow"
+       self.background_colour = "yellow"
 
     #Converter Main Screen GUI.....
-    self.converter_frame = Frame(width=600, height=600, bg=background_colour)
-    self.converter_frame.grid()
+        self.converter_frame = Frame(width=600, height=600, bg=background_colour)
+        self.converter_frame.grid()
 
 #main routine
 if__name__ == "__main__"
     root = Tk()
     root.title("Temperature Converter")
+    root.mainloop()
