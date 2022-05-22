5d0824a36a52e73dd6539bf175e2c51208924495
teise <teisteu@gmail.com>
2022-05-23 09:43:23 +1200

    style used to format

--- 01_help_GUI_v2.py
+++ 01_help_GUI_v2.py
@@ -3,18 +3,26 @@ from tkinter import ttk
 import random
 
 
-
 class Converter:
-    def__init__(self):
-    #Formatting variables....
-       self.background_colour = "yellow"
+    def __init__(self, parent):
+        # Formatting variables....
+        background_colour = "yellow"
 
-    #Converter Main Screen GUI.....
-        self.converter_frame = Frame(width=600, height=600, bg=background_colour)
+        # style configuration
+        style = ttk.Style()
+        style.configure('Frame1.TLabel', background=background_colour, foreground='black')
+
+        # Converter Main Screen GUI.....
+        self.converter_frame = ttk.Frame(style="Frame1.TLabel")
         self.converter_frame.grid()
+        self.temp_converter_label = Label(self.converter_frame, text="Help", bg=background_colour)
+        self.temp_converter_label.grid(row=0, column=0, padx=20, pady=20)
+        # self.help_button.Button(root, text="Help button")
 
-#main routine
-if__name__ == "__main__"
+
+# main routine
+if __name__ == "__main__":
     root = Tk()
-    root.title("Temperature Converter")
+    root.title("Help")
+    something = Converter(root)
     root.mainloop()
